087473702f9ba2463632faea1b2f17367a99224a
sarah almutari <sara0moha78@gmail.com>
2022-03-16 20:09:56 +0300

    donee

--- d5-cw3.playground/Contents.swift
+++ d5-cw3.playground/Contents.swift
@@ -1,3 +1,40 @@
 import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+func printuser(){
+    print("xuvernon")
+}
+printuser()
+func grade(first:Int,second:Int,final:Int) -> String {
+    let grade = first + second + final
+    if ( grade >= 90 && grade <= 100){
+        return "A"
+    }
+    else if ( grade >= 80 && grade <= 89 ){
+        return "B"
+    }
+else if ( grade >= 70 && grade <= 79 ){
+        return "C"
+    }
+else if ( grade >= 60 && grade <= 69 ){
+        return "D"
+    }
+
+    else{
+        return "F"
+        }
+}
+print(grade(first: 0, second: 0, final: 4))
+
+func name() -> String{
+    return "sarah mohammed"
+}
+var i = name()
+print(i)
+
+func numbers(num1:Int,num2:Int){
+    print(num1+num2)
+    print(num1-num2)
+    print(num1*num2)
+}
+numbers(num1: 7, num2: 2)
